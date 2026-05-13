54c1feb40a5d7e3bdd723ef33f6c16f212bb8cbd
YZM365 <2926921501@qq.com>
2026-05-13 21:04:01 +0800

    mission complete

--- src/tag_voice.cpp
+++ src/tag_voice.cpp
@@ -1,5 +1,11 @@
 #include <ros/ros.h>
+#include <move_base_msgs/MoveBaseAction.h>
 #include <std_msgs/String.h>
+#include <actionlib/client/simple_action_client.h>
+#include <iostream>
+#include <tf2/LinearMath/Quaternion.h>
+#include <geometry_msgs/Quaternion.h>
+#include <cmath>
 #include <apriltag_ros/AprilTagDetectionArray.h>
 
 ros::Publisher voice_pub;
@@ -12,6 +18,7 @@ void tagCallback(const apriltag_ros::AprilTagDetectionArray::ConstPtr& msg)
     // 核心：只有flag为false时，才执行单次检测（锁死时直接退出）
     if (spoken_flag)
     {
+    	ROS_INFO("111111111");
         return;
     }
 
@@ -56,6 +63,12 @@ int main(int argc, char** argv)
     voice_pub = nh.advertise<std_msgs::String>("/talk", 10);
     ros::Subscriber tag_sub = nh.subscribe("/tag_detections", 10, tagCallback);
 
+    ros::Time start_wait = ros::Time::now();
+    while (ros::Time::now() - start_wait < ros::Duration(5.0)) {
+        ros::spinOnce();  // 正常处理标签检测+语音
+        ros::Rate(10).sleep();
+    }
+    
     ros::spin();
     return 0;
 }
