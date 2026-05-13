7f442ce264c9e5122fcfdfcd649b2ae9ed6af560
YZM365 <2926921501@qq.com>
2026-05-13 17:36:07 +0800

    add tag_voice_node

--- src/final.cpp
+++ src/final.cpp
@@ -24,9 +24,9 @@ int main(int argc, char **argv)
     tf2::Quaternion quaternion;
 
     // 待发送的目标点1 在 map 坐标系下的坐标位置（将上一节标定的位置填写到对应的x，y，yaw）
-    quaternion.setRPY(0, 0, 0); // 最后一个数对应 yaw 角，正前方向量顺时针旋转
-    goal1.target_pose.pose.position.x =2.421680; //x，相对于建图起点前后的值，前正后负
-    goal1.target_pose.pose.position.y =-1.811785; //y，相对于建图起点左右的值，左正右负
+    quaternion.setRPY(0, 0, -1.708440); // 最后一个数对应 yaw 角，正前方向量顺时针旋转
+    goal1.target_pose.pose.position.x =2.270775; //x，相对于建图起点前后的值，前正后负
+    goal1.target_pose.pose.position.y =-1.998624; //y，相对于建图起点左右的值，左正右负
     goal1.target_pose.pose.orientation.z = quaternion.z();
     goal1.target_pose.pose.orientation.w = quaternion.w();
     goal1.target_pose.header.frame_id = "map";
@@ -44,9 +44,9 @@ int main(int argc, char **argv)
     }
 
     // 待发送的目标点2 在 map 坐标系下的坐标位置
-    quaternion.setRPY(0, 0, -1.5707); // 最后一个数对应 yaw 角，正前方向量顺时针旋转
-    goal2.target_pose.pose.position.x = 1.182802;
-    goal2.target_pose.pose.position.y = -1.155454;
+    quaternion.setRPY(0, 0, 1.311625); // 最后一个数对应 yaw 角，正前方向量顺时针旋转
+    goal2.target_pose.pose.position.x = 1.896779;
+    goal2.target_pose.pose.position.y = -3.601497;
     goal2.target_pose.pose.orientation.z = quaternion.z();
     goal2.target_pose.pose.orientation.w = quaternion.w();
     goal2.target_pose.header.frame_id = "map";
@@ -64,10 +64,11 @@ int main(int argc, char **argv)
     }
 
     // 待发送的 home 目标点 在 map 坐标系下的坐标位置
-    goal3.target_pose.pose.position.x = 0.0;
-    goal3.target_pose.pose.position.y = 0.0;
-    goal3.target_pose.pose.orientation.z = 0.0;
-    goal3.target_pose.pose.orientation.w = 1.0;
+    quaternion.setRPY(0, 0, -0.322329);
+    goal3.target_pose.pose.position.x = -1.150072;
+    goal3.target_pose.pose.position.y = -2.032008;
+    goal3.target_pose.pose.orientation.z = quaternion.z();
+    goal3.target_pose.pose.orientation.w = quaternion.w();
     goal3.target_pose.header.frame_id = "map";
     goal3.target_pose.header.stamp = ros::Time::now();
     ac.sendGoal(goal3);
